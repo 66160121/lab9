5068c1ee7272d65267317cca0ccd929e26f4d491
chutikarn 66160121@go.buu.ac.th <66160121@go.buu.ac.th>
2025-02-19 10:34:05 +0700

    แก้ไขuser.js

--- user-detail.js
+++ user-detail.js
@@ -1,10 +1,19 @@
-document.addEventListener("DOMContentLoaded", async () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const userId = urlParams.get("id");
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
-    const user = await response.json();
-    document.getElementById("user-detail").innerHTML = `<h2>${user.name}</h2><p>${user.email}</p>`;
-    document.getElementById("view-posts").addEventListener("click", () => {
-        window.location.href = `user-posts.html?id=${user.id}`;
-    });
-});
+const params = new URLSearchParams(window.location.search);
+const userId = params.get("id");
+
+if (userId) {
+      fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+      .then((response) => response.json())
+      .then((user) => {
+            document.getElementById("user-detail").innerHTML = `
+                        <h2>${user.name}</h2>
+                        <p><strong>อีเมล<br></strong> ${user.email}</p>
+                        <p><strong>เบอร์โทรศัพท์<br></strong> ${user.phone}</p>
+                        <p><strong>เว็บไซต์<br></strong> ${user.website}</a></p>
+                        <p><strong>ที่อยู่<br></strong> ${user.address.street}, ${user.address.city}</p>
+                        <p><strong>บริษัท<br></strong> ${user.company.name}</p>
+                        <button id="view-posts" class="view-posts-btn"><a href="user-posts.html?id=${user.id}">ดูโพสต์ทั้งหมด</a></button>
+ 
+                  `;
+           });
+}

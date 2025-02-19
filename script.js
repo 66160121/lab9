30b6722e4751edbd3d3e039684b21249a6056102
chutikarn 66160121@go.buu.ac.th <66160121@go.buu.ac.th>
2025-02-19 10:31:26 +0700

    แก้ไขscript

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-async function fetchUsers() {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        const users = await response.json();
-        const userList = document.getElementById('user-list');
-        users.forEach(user => {
-            const div = document.createElement('div');
-            div.classList.add('user-item');
-            div.innerHTML = `<a href="user-detail.html?id=${user.id}">${user.name}</a>`;
-            userList.appendChild(div);
-        });
-    } catch (error) {
-        console.error('Error fetching users:', error);
-    }
-}
-document.addEventListener('DOMContentLoaded', fetchUsers);
+fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+            document.getElementById("user").innerHTML = data
+            .map(
+                  (u) =>
+                        `<div class="container">
+                              <a class="text-a" href="user-detail.html?id=${u.id}">
+                                    ${u.name}<br>
+                                    ${u.email}
+                              </a>
+                        </div>`
+            ).join("");
+      });
